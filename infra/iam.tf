a2ec515d8ee1a16fd109c27f28a605af0a3919a5
Will Payne <will@payne.uk>
2026-09-24 09:20:03 +0000

    Allow Terraform to tag execution roles

--- infra/iam.tf
+++ infra/iam.tf
@@ -134,7 +134,7 @@ resource "aws_iam_policy" "github_actions" {
         Action = [
           "apigateway:*", "cloudformation:DescribeStacks", "cloudfront:*", "cognito-idp:*",
           "ec2:Describe*", "events:*", "iam:CreateRole", "iam:DeleteRole", "iam:Get*",
-          "iam:List*", "iam:PassRole", "iam:PutRolePolicy", "iam:DeleteRolePolicy",
+          "iam:List*", "iam:PassRole", "iam:TagRole", "iam:UntagRole", "iam:PutRolePolicy", "iam:DeleteRolePolicy",
           "iam:AttachRolePolicy", "iam:DetachRolePolicy", "lambda:*", "logs:*", "rds:*",
           "s3:*", "sqs:*", "sts:GetCallerIdentity"
         ]
