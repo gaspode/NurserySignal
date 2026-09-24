bc80f7010b39ad74d9c010bd06b9aee496519ea6
Will Payne <will@payne.uk>
2026-09-24 08:23:09 +0000

    Fix GitHub OIDC workflow trust scope

--- infra/iam.tf
+++ infra/iam.tf
@@ -67,9 +67,14 @@ data "aws_iam_policy_document" "github_assume_role" {
       values   = ["sts.amazonaws.com"]
     }
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:ref:refs/heads/main"]
+      values   = ["repo:${var.github_repository}:*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:job_workflow_ref"
+      values   = ["${var.github_repository}/.github/workflows/deploy.yml@refs/heads/main"]
     }
   }
 }
