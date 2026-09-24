af6b0a9dbdc0643805dbd0c6deaa7a1d2a4b09b5
Will Payne <will@payne.uk>
2026-09-24 11:12:06 +0000

    Allow planning secret state reads

--- infra/iam.tf
+++ infra/iam.tf
@@ -160,8 +160,11 @@ resource "aws_iam_policy" "github_actions" {
         }
       },
       {
-        Effect   = "Allow"
-        Action   = ["secretsmanager:TagResource", "secretsmanager:UntagResource"]
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:DescribeSecret", "secretsmanager:TagResource",
+          "secretsmanager:UntagResource"
+        ]
         Resource = "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:${local.name_prefix}/planning-provider-*"
       },
       {
